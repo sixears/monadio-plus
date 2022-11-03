f6aa2c42335942a0beba5858173919e44f764802
Mr. Fluffy <git@sixearsm>
2022-11-03 11:22:16 +0000

    playing with flakes

--- flake.nix
+++ flake.nix
@@ -8,9 +8,11 @@
     base1t.url          = "github:sixears/base1t/r0.0.5.7";
     containers-plus.url = "github:sixears/containers-plus/r0.0.10.8";
     env-plus.url        = "github:sixears/env-plus/r1.0.7.7";
+
     exited.url          = "github:sixears/exited/r1.0.4.7";
     fpath.url           = "github:sixears/fpath/r1.3.2.7";
     fstat.url           = "github:sixears/fstat/r1.0.2.6";
+
     monaderror-io.url   = "github:sixears/monaderror-io/r1.2.5.6";
     more-unicode.url    = "github:sixears/more-unicode/r0.0.17.4";
     natural.url         = "github:sixears/natural/r0.0.1.4";
@@ -18,12 +20,21 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, build-utils
-            , base1t, containers-plus, env-plus, exited, fpath, fstat
-            , monaderror-io, more-unicode, natural, tasty-plus }:
+            , base1t, containers-plus, env-plus
+            , exited
+            , fpath
+            , fstat
+            , monaderror-io, more-unicode
+            , natural, tasty-plus
+            }:
     build-utils.lib.hOutputs self nixpkgs "monadio-plus" {
       deps = {
-        inherit base1t containers-plus env-plus exited fpath fstat monaderror-io
-                more-unicode natural tasty-plus;
+        inherit base1t containers-plus env-plus exited
+          fpath
+          fstat
+          monaderror-io more-unicode
+          natural tasty-plus
+        ;
       };
       ghc = p: p.ghc8107; # for tfmt
     };
