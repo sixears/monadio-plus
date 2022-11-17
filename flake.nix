adcab69ba536f53a0f5ac267f61a092b79c1c4f2
Mr. Fluffy <git@sixearsm>
2022-11-17 21:07:45 +0000

    tfmt->0.2.7.15

--- flake.nix
+++ flake.nix
@@ -5,14 +5,14 @@
     nixpkgs.url     = "github:nixos/nixpkgs/be44bf67"; # nixos-22.05 2022-10-15
     build-utils.url = "github:sixears/flake-build-utils/r1.0.0.11";
 
-    base1t.url          = "github:sixears/base1t/r0.0.5.16";
-    env-plus.url        = "github:sixears/env-plus/r1.0.7.16";
+    base1t.url          = "github:sixears/base1t/r0.0.5.17";
+    env-plus.url        = "github:sixears/env-plus/r1.0.7.17";
 
     exited.url          = "github:sixears/exited/r1.0.4.14";
-    fpath.url           = "github:sixears/fpath/r1.3.2.16";
-    fstat.url           = "github:sixears/fstat/r1.0.2.13";
+    fpath.url           = "github:sixears/fpath/r1.3.2.17";
+    fstat.url           = "github:sixears/fstat/r1.0.2.14";
 
-    containers-plus.url = "github:sixears/containers-plus/r0.0.10.17";
+    containers-plus.url = "github:sixears/containers-plus/r0.0.10.18";
     monaderror-io.url   = "github:sixears/monaderror-io/r1.2.5.12";
     more-unicode.url    = "github:sixears/more-unicode/r0.0.17.8";
     natural.url         = "github:sixears/natural/r0.0.1.9";
