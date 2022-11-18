4112412763f9b91f67e601cd5032954f0b7aebea
Mr. Fluffy <git@sixearsm>
2022-11-18 21:17:15 +0000

    index->1.0.1.17;base0t->0.0.1.10;number->1.1.2.11;base0->0.0.4.8;more-unicode->0.0.17.9;flake-build-utils->1.0.0.12

--- flake.nix
+++ flake.nix
@@ -5,18 +5,18 @@
     nixpkgs.url     = "github:nixos/nixpkgs/be44bf67"; # nixos-22.05 2022-10-15
     build-utils.url = "github:sixears/flake-build-utils/r1.0.0.12";
 
-    base1t.url          = github:sixears/base1t/r0.0.5.24;
-    env-plus.url        = github:sixears/env-plus/r1.0.7.24;
+    base1t.url          = github:sixears/base1t/r0.0.5.25;
+    env-plus.url        = github:sixears/env-plus/r1.0.7.25;
 
-    exited.url          = "github:sixears/exited/r1.0.4.16";
-    fpath.url           = github:sixears/fpath/r1.3.2.23;
-    fstat.url           = github:sixears/fstat/r1.0.2.17;
+    exited.url          = github:sixears/exited/r1.0.4.17;
+    fpath.url           = github:sixears/fpath/r1.3.2.24;
+    fstat.url           = github:sixears/fstat/r1.0.2.18;
 
-    containers-plus.url = github:sixears/containers-plus/r0.0.10.25;
-    monaderror-io.url   = "github:sixears/monaderror-io/r1.2.5.13";
+    containers-plus.url = github:sixears/containers-plus/r0.0.10.26;
+    monaderror-io.url   = github:sixears/monaderror-io/r1.2.5.14;
     more-unicode.url    = "github:sixears/more-unicode/r0.0.17.9";
     natural.url         = "github:sixears/natural/r0.0.1.10";
-    tasty-plus.url      = "github:sixears/tasty-plus/r1.5.2.16";
+    tasty-plus.url      = github:sixears/tasty-plus/r1.5.2.17;
   };
 
   outputs = { self, nixpkgs, build-utils
