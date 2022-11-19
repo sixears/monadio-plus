842ad59ad66670ecd302b1aa1557b95e79a8515b
Mr. Fluffy <git@sixearsm>
2022-11-19 21:44:53 +0000

    tasty-plus->1.5.2.22

--- flake.nix
+++ flake.nix
@@ -5,18 +5,18 @@
     nixpkgs.url     = github:nixos/nixpkgs/be44bf67; # nixos-22.05 2022-10-15
     build-utils.url = github:sixears/flake-build-utils/r1.0.0.13;
 
-    base1t.url          = github:sixears/base1t/r0.0.5.33;
-    env-plus.url        = github:sixears/env-plus/r1.0.7.34;
+    base1t.url          = github:sixears/base1t/r0.0.5.34;
+    env-plus.url        = github:sixears/env-plus/r1.0.7.35;
 
     exited.url          = github:sixears/exited/r1.0.4.21;
-    fpath.url           = github:sixears/fpath/r1.3.2.36;
+    fpath.url           = github:sixears/fpath/r1.3.2.37;
     fstat.url           = github:sixears/fstat/r1.0.2.24;
 
-    containers-plus.url = github:sixears/containers-plus/r0.0.10.36;
+    containers-plus.url = github:sixears/containers-plus/r0.0.10.37;
     monaderror-io.url   = github:sixears/monaderror-io/r1.2.5.18;
     more-unicode.url    = github:sixears/more-unicode/r0.0.17.11;
     natural.url         = github:sixears/natural/r0.0.1.13;
-    tasty-plus.url      = github:sixears/tasty-plus/r1.5.2.21;
+    tasty-plus.url      = github:sixears/tasty-plus/r1.5.2.22;
   };
 
   outputs = { self, nixpkgs, build-utils
@@ -43,7 +43,7 @@
                     }:
         mkDerivation {
           pname = "monadio-plus";
-          version = "2.5.1.46";
+          version = "2.5.1.47";
           src = ./.;
           isLibrary = true;
           isExecutable = true;
